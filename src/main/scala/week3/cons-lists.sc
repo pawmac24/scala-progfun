3f3608c740cca92060a55e9dd544048fa969d9cc
Pawel M <pawmac24@gmail.com>
2017-03-18 18:53:08 +0100

    Cons-list project

---
+++ src/main/scala/week3/cons-lists.sc
@@ -0,0 +1,13 @@
+/*
+Cons-list - Immutable linked list data structure
+It is constructed from two blocks:
+Nill - the empty list
+Cons - a cell containing an element
+  and the remainder of the list
+*/
+
+object consList{
+  List(1, 2, 3)
+  List(List(true, false), List(3))
+}
+
---
+++ src/main/scala/week4/List.scala
@@ -0,0 +1,23 @@
+package week4
+
+import java.util.NoSuchElementException
+
+/**
+  * Created by PawelM on 2017-03-18.
+  */
+trait List[T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T]{
+  def isEmpty = false
+
+}
+
+class Nil[T] extends List[T]{
+  def isEmpty: Boolean = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
